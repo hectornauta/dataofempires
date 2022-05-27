e062ad3ea50c416b0b695b6410c70371ee4047ba
Hectornauta <hemilianop13@ca.frre.utn.edu.ar>
2022-05-26 23:37:58 -0300

    Fix

--- report_viewer.py
+++ report_viewer.py
@@ -175,6 +175,7 @@ def civ_vs_civ(chosen_civ=-1, ladder='3A'):
     return figure_civ_vs_civ
 
 def elo_distribution(ladder=3):
+    return None
     '''
     FILE = f'{DIR}/sql/get_players_elo.sql'
     dataframe_players_elo = sql_functions.get_sql_results(FILE, ladder)
